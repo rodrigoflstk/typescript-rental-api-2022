ea220b8db234d032418ccbeabd17b13f36fb982f
rodrigo <jrodrigo0592@gmail.com>
2022-11-02 16:41:14 -0300

    fix: some code updates

--- prisma/migrations/20221101204312_alter_table_user_2/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "user" ALTER COLUMN "admin" SET DEFAULT false;
--- prisma/migrations/20221102183653_user_car_spec_models/migration.sql
+++ prisma/migrations/20221102183653_user_car_spec_models/migration.sql
@@ -17,3 +17,20 @@ CREATE TABLE "specifications" (
 
     CONSTRAINT "specifications_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "driver_license" TEXT NOT NULL,
+    "admin" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
